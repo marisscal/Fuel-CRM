aa25075b254a8bbcf7191849c3d4f2b8b76d273b
Mariscal <marisscal@users.noreply.github.com>
2015-08-10 07:25:00 -0600

    Create index.php

---
+++ v/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Fuel CRM</title>
+	<script language="javascript">location.href = "../index.php";</script>
+</head>
+<body>
+	
+</body>
+</html>
